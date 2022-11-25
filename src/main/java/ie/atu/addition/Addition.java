b104fa4c8fa13180f612487f67165dbfd3de975c
Dylan Walsh <L00163199@atu.ie>
2022-11-25 08:45:04 +0000

    test
    test

---
+++ src/main/java/ie/atu/addition/Addition.java
@@ -0,0 +1,8 @@
+// Simple class to demonstrate automated JUnit tests
+package ie.atu.addition;
+public class Addition {
+
+    public int addNumber(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+}
---
+++ src/test/java/ie/atu/addition/AdditionTest.java
@@ -0,0 +1,15 @@
+package ie.atu.addition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdditionTest {
+    @Test
+    @DisplayName("Addition test")
+    void addsTwoNumbers() {
+        Addition add = new Addition();
+        assertEquals(10, add.addNumber(5, 5), "5 + 5 should give 10");
+    }
+}
