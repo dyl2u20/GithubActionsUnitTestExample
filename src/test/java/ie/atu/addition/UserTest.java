df6bbecca3536c7fecac5f495c483f05e8a966ed
Dylan Walsh <L00163199@atu.ie>
2022-12-09 08:16:47 +0000

    restructure pom.xml

---
+++ src/test/java/ie/atu/addition/UserTest.java
@@ -0,0 +1,19 @@
+package ie.atu.addition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    @DisplayName("Username Setter Test")
+    void testSetter() {
+        User newUser = new User("Adrian");
+
+        newUser.setUsername("Ciara");
+        assertEquals("Ciara", newUser.getUsername(), "Username should be Ciara");
+    }
+
+}
