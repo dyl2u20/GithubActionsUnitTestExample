51b5504727a3adbeeba0cad49a54d18eb943008c
Dylan Walsh <L00163199@atu.ie>
2022-12-09 09:48:25 +0000

    restructure pom.xml

---
+++ Code/src/main/java/Booking.java
@@ -0,0 +1,74 @@
+/**
+ * Booking class for comic booking system
+ * @author RogueATU
+ */
+public class Booking {
+
+    //private Date bookingDate;
+    /**
+     * The customer making the booking
+     */
+    private Customer customer;
+    /**
+     * The workshop that is being booked
+     */
+    private String workshop;
+    /**
+     * The number of tickets being purchased
+     */
+    private int tickets;
+
+    public Booking(Customer customer, Workshop workshop, int tickets){
+
+    }
+
+    /**
+     * Getter for Customer
+     *
+     * @return customer
+     */
+    public Customer getCustomer(){
+        return customer;
+    }
+
+    /**
+     * Getter for Workshop
+     *
+     * @return workshop
+     */
+    public String getWorkshop(){
+        return workshop;
+    }
+
+    /**
+     * Getter for tickets
+     *
+     * @return tickets
+     */
+    public int getTickets(){
+        return tickets;
+    }
+    /**
+     * To string method for Booking
+     * @return the customer, workshop and amount of tickets
+     */
+
+
+    public void setTickets(int tickets){
+        this.tickets = tickets;
+    }
+
+    public void setCustomer(String username){
+        // Will amend this later
+        //this.username = username;
+    }
+
+    public void setWorkshop(String workshop){
+        this.workshop = workshop;
+    }
+    @Override
+    public String toString(){
+        return this.getCustomer() + " booked " + tickets+ " for " + workshop;
+    }
+
+}
